b33fdd5710e26e09da92889434b7fe6bab2c1539
MadSkills88 <anthonybao1999@gmail.com>
2019-02-16 16:48:55 -0800

    updated analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -47,12 +47,15 @@ diff_left_eye_y = [j-i for i, j in zip(left_eye_y[:-1], left_eye_y[1:])]
 diff_right_eye_t = [j-i for i, j in zip(right_eye_t[:-1], right_eye_t[1:])]
 diff_left_eye_t = [j-i for i, j in zip(left_eye_t[:-1], left_eye_t[1:])]
 
+print(len(diff_left_eye_t))
+print(len(diff_left_eye_x))
+
 v_right_eye_x = [x/y for x, y in zip(diff_right_eye_x, diff_right_eye_t)]
 v_right_eye_y = [x/y for x, y in zip(diff_right_eye_y, diff_right_eye_t)]
 v_left_eye_x = [x/y for x, y in zip(diff_left_eye_x, diff_left_eye_t)]
 v_left_eye_y = [x/y for x, y in zip(diff_left_eye_y, diff_left_eye_t)]
 
-
+print(len(v_left_eye_x))
 
 ## Raw position plots and scatter
 # line_lx, = plt.plot(left_eye_t, left_eye_x, 'g', label="left eye x")
@@ -72,12 +75,27 @@ print(left_eye_x)
 
 print(diff_left_eye_t)
 print(diff_left_eye_x)
-## Differential plots and scatter
-line_lx, = plt.plot(diff_left_eye_t, diff_left_eye_x, c='g', label="left eye x")
-line_ly, = plt.plot(diff_left_eye_t, diff_left_eye_y, c='b', label="left eye y")
 
-line_rx, = plt.plot(diff_right_eye_t, diff_right_eye_x, c='r', label="right eye x")
-line_ry, = plt.plot(diff_right_eye_t, diff_right_eye_y, c='orange', label="right eye y")
+print(v_left_eye_x)
+## Differential plots and scatter
+# line_lx, = plt.plot(diff_left_eye_t, diff_left_eye_x, c='g', label="left eye x")
+# line_ly, = plt.plot(diff_left_eye_t, diff_left_eye_y, c='b', label="left eye y")
+#
+# line_rx, = plt.plot(diff_right_eye_t, diff_right_eye_x, c='r', label="right eye x")
+# line_ry, = plt.plot(diff_right_eye_t, diff_right_eye_y, c='orange', label="right eye y")
+
+# plt.scatter(diff_left_eye_t, diff_left_eye_x, c='g')
+# plt.scatter(diff_left_eye_t, diff_left_eye_y, c='b')
+#
+# plt.scatter(diff_right_eye_t, diff_right_eye_x, c='r')
+# plt.scatter(diff_right_eye_t, diff_right_eye_y, c='orange')
+
+## Velocity plots and scatter
+line_lx, = plt.plot(left_eye_t[1:], v_left_eye_x, c='g', label="left eye x")
+line_ly, = plt.plot(left_eye_t[1:], v_left_eye_y, c='b', label="left eye y")
+
+line_rx, = plt.plot(right_eye_t[1:], v_right_eye_x, c='r', label="right eye x")
+line_ry, = plt.plot(right_eye_t[1:], v_right_eye_y, c='orange', label="right eye y")
 
 # plt.scatter(diff_left_eye_t, diff_left_eye_x, c='g')
 # plt.scatter(diff_left_eye_t, diff_left_eye_y, c='b')
